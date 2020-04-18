652347fdaa956fa17e3ee512c00c7021612c0a7c
Duke <doukaskostas92@gmail.com>
2020-04-18 18:20:03 +0300

    Made the site responsive

--- main.js
+++ main.js
@@ -3,3 +3,12 @@ window.addEventListener("load", function () {
   loader.className += " hidden";
   setTimeout(() => (loader.style.display = "none"), 2000);
 });
+function menuClick(x) {
+  x.classList.toggle("change");
+  const menu = document.querySelector(".navigation");
+  if (menu.offsetWidth == 0) {
+    menu.style.width = "300px";
+  } else {
+    menu.style.width = "0px";
+  }
+}
